7f809b5e7ca224f6354728e4c56ab03e313763c9
TARIK BAYRAM <tkbayram7@gmail.com>
2023-01-20 21:26:09 +0300

    Added : Core Project

---
+++ Business/Abstract/IBrandService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IBrandService
+    {
+        void Add(Brand entity);
+        void Delete(Brand entity);
+        void Update(Brand entity);
+        List<Brand> GetAll();
+        Brand GetById(int id);
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,11 +5,34 @@ using Entities.Concrete;
 using System;
 
 CarManager carManager = new CarManager(new EfCarDal());
+BrandManager brandManager = new BrandManager(new EfBrandDal());
+ColorManager colorManager = new ColorManager(new EfColorDal());
 
-Car car1 = new Car() { CarId=2,BrandId=1,ColorId=1,DailyPrice=100000,ModelYear=1999,Description="iyi", CarName="BMW"};
-carManager.Delete(car1);
+var brand1 = new List<Brand>()
+{
+    new Brand {BrandId=1,Name="BMW"},
+    new Brand {BrandId=2,Name="RENAULT"},
 
-//foreach (var car in carManager.GetCarsByColorId(1))
-//{
-//    Console.WriteLine(car.ModelYear);
-//}
+};
+
+var color1 = new List<Color>()
+{
+    new Color {ColorId=1,Name="Siyah"},
+    new Color {ColorId=2,Name="Kırmızı"},
+    new Color {ColorId=3,Name="Beyaz"},
+
+};
+
+var car1 = new List<Car>() {
+    new Car{CarId = 1, BrandId = 1, ColorId = 1, DailyPrice = 100000, ModelYear = 1999, Description = "iyi", CarName = "BMW 335ci"},
+    new Car { CarId = 2, BrandId = 1, ColorId = 3, DailyPrice = 103000, ModelYear = 2000, Description = "kötü", CarName = "BMW 418i" },
+    new Car { CarId = 3, BrandId = 2, ColorId = 3, DailyPrice = 145000, ModelYear = 2001, Description = "iyi", CarName = "RENAULT Fluence" },
+    new Car { CarId = 4, BrandId = 2, ColorId = 3, DailyPrice = 146000, ModelYear = 2002, Description = "kötü", CarName = "RENAULT Clio" },
+    new Car { CarId = 5, BrandId = 1, ColorId = 2, DailyPrice = 174000, ModelYear = 2003, Description = "iyi", CarName = "BMW M8" }
+};
+
+
+foreach (var car in carManager.GetCarDetails())
+{
+    Console.WriteLine("{0} / {1} / {2}", car.CarName, car.BrandName, car.ColorName);
+}
