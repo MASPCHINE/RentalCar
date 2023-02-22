d83d66281bb8a0beb25098a228b82b34afc4952a
TARIK BAYRAM <tkbayram7@gmail.com>
2023-02-22 16:51:28 +0300

    Updated : JWT Helper

--- Core/Utilities/Security/JWT/ITokenHelper.cs
+++ Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Entities;
+using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace Core.Utilities.Security.JWT
 {
-    internal interface ITokenHelper
+    public interface ITokenHelper
     {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
     }
 }
